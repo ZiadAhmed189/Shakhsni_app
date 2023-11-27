8ef7875fbe6bd80db6463ab1a75513efe1615c5f
ZiadAhmed189 <123209721+ZiadAhmed189@users.noreply.github.com>
2023-11-27 20:37:26 +0200

    summary

--- shakhni_app/lib/main.dart
+++ shakhni_app/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Text("Ziad"),
+    );
+  }
 
 }
